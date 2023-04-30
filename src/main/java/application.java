7be441bac358175f8e9195bb8f71b124edaaef99
Pedro Ramalho <pedro_hrr@hotmail.com>
2023-04-30 01:58:31 -0300

    ideia melhorada

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -6,13 +6,14 @@ public class application {
 
 
         // criar conjunto para familia do prato e add o prato dentro
-        // passar todos os conjuntos na hora das peruntas
+        // passar todos os strings  do conjuto  na hora da pergunta
         // caso todos falso perguntar se é o prato default
-        // caso seja algum conjunto , passar todos os pratos dentro do conjunto
+        // caso seja algum do  conjunto , passar todos os pratos dentro do conjunto
 
-        // caso for add um conjunto novo, vefericar se já existe
+
+        // caso nao for  add um conjunto novo, vefericar se já existe
         // caso exista add o prato novo dentro do conjunto
-        // caso o prato ja exista nao fazer nada
+        // caso o prato ja exista nao fazer nada e finalizar
 
 
 
