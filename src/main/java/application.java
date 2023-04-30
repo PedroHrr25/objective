7f711c390b1ae214ca4761b266775986c988c397
Pedro Ramalho <pedro_hrr@hotmail.com>
2023-04-30 01:21:30 -0300

    ideias

---
+++ src/main/java/application.java
@@ -0,0 +1,25 @@
+public class application {
+
+    public static void main(String[] args) {
+
+
+
+
+        // criar conjunto para familia do prato e add o prato dentro
+        // passar todos os conjuntos na hora das peruntas
+        // caso todos falso perguntar se é o prato default
+        // caso seja algum conjunto , passar todos os pratos dentro do conjunto
+
+        // caso for add um conjunto novo, vefericar se já existe
+        // caso exista add o prato novo dentro do conjunto
+        // caso o prato ja exista nao fazer nada
+
+
+
+
+
+
+
+
+    }
+}
