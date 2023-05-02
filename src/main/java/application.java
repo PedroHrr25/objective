d0af44b613fc28e58f16ca9e4809a32c76c36c2d
Pedro Ramalho <pedro_hrr@hotmail.com>
2023-05-02 12:14:57 -0300

    algumas correcoes

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,17 +1,12 @@
 import javax.swing.*;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
 
 public class application {
-
+    static HashMap<TiposPratos, HashSet<Pratos>> todos = new HashMap<>();
     public static void main(String[] args) {
 
-
-        JOptionPane.showMessageDialog(null, "Pense em um prato que gosta");
-
         Object[] options = {"Sim", "Não"};
-        HashMap<TiposPratos, HashSet<Pratos>> todos = new HashMap<>();
         HashSet<Pratos> massa = new HashSet<>();
         massa.add(new Pratos("lasanha"));
         massa.add(new Pratos("coxinha"));
@@ -19,43 +14,66 @@ public class application {
         todos.put(new TiposPratos("massa"), massa);
         String nome;
         String nome2;
+        int opcao;
+        int opcao2;
 
 
         while (true) {
+            boolean acerto = false;
+            JOptionPane.showMessageDialog(null, "Pense em um prato que gosta");
             for (TiposPratos list : todos.keySet()) {
-                int opcao = JOptionPane.showOptionDialog(null, "O tipo de prato  que você pensou é " + list + " ?", "Confirmação", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
+                opcao = JOptionPane.showOptionDialog(null, "O tipo de prato  que você pensou é " + list + " ?", "Confirmação", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
                 if (opcao == JOptionPane.YES_OPTION) {
 
                     HashSet<Pratos> conjunto = todos.get(list);
 
+                    boolean achou = false;
                     for (Pratos pratos : conjunto) {
-                        int opcao2 = JOptionPane.showOptionDialog(null, "O Prato que você pensou é " + pratos, "Confirmação", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
-                        if (opcao2 == JOptionPane.YES_OPTION) {
-                            JOptionPane.showMessageDialog(null, "Seu Prato é " + pratos);
+                        opcao2 = JOptionPane.showOptionDialog(null, "O Prato que você pensou é " + pratos, "Confirmação", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
+                        switch (opcao2) {
+                            case JOptionPane.YES_OPTION: {
+                                JOptionPane.showMessageDialog(null, "Seu Prato é " + pratos);
+                                achou = true;
+                                acerto = true;
+                                break;
+                            }
+                            case JOptionPane.NO_OPTION: {
+                                break;
+                            }
+                            case JOptionPane.CANCEL_OPTION: {
+                                System.exit(0);
+                            }
+                        }
+                        if (achou) {
                             break;
                         }
                     }
                 }
-                if (opcao == JOptionPane.NO_OPTION) {
-                } else {
-                    System.exit(0);
-                }
-            }
-            nome = JOptionPane.showInputDialog(null, "Qual prato você pensou? ");
-            nome2 = JOptionPane.showInputDialog(null, nome + " é _____ mas não é bolo de chocolate");
 
+            }
+            if (!acerto) {
+                nome = JOptionPane.showInputDialog(null, "Qual prato você pensou? ");
+                nome2 = JOptionPane.showInputDialog(null, nome + " é _____ mas não é bolo de chocolate");
+                addNovoPrato(nome, nome2);
+            }
+            }
+
+        }
+        public static void addNovoPrato (String nome, String nome2){
             if (todos.containsKey(nome2)) {
                 HashSet<Pratos> conjunto = todos.get(nome2);
+                todos.remove(nome2);
                 conjunto.add(new Pratos(nome));
+                todos.put(new TiposPratos(nome2), conjunto);
             } else {
                 HashSet<Pratos> novoConjunto = new HashSet<>();
                 novoConjunto.add(new Pratos(nome));
                 todos.put(new TiposPratos(nome2), novoConjunto);
             }
 
+
         }
     }
-}
 
 
 
